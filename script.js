feabd17ab16c1a2e2a15500b5366dbb29a8db3aa
sagarpanwar1 <52241021+sagarpanwar1@users.noreply.github.com>
2021-09-21 23:54:55 +0530

    all done

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
  var btnClick = document.querySelector ("#btn-click");
  var textArea = document.querySelector ("#mytext");
     var outputDiv = document.querySelector("#output");
+
+var serverUrl = "https://api.funtranslations.com/translate/leetspeak.json"
+
 //  btnClick.addEventListener("click", function clickEventHandler()
 //  {
 //         console.log("clicked");
@@ -13,11 +16,32 @@
 
 // console.log(textArea);
 
+
+
+function getTranslationURL (input){
+    return serverUrl + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
 function clickHandler ()
 {
     //  console.log("clicked");
     // console.log(textArea.value);
-           outputDiv.innerText = "dfdfdfddfasdfadfad " + textArea.value;   
+        //    outputDiv.innerText = "dfdfdfddfasdfadfad " + textArea.value;   
+        var InputText = mytext.value;
+           
+        fetch(getTranslationURL(InputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
+
 };
 
 btnClick.addEventListener("click",clickHandler)
